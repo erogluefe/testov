2b3b1864c4a16f2eb8a30e1e1882cf3cd33cf08f
Efe Eroğlu <efe.eroglu1996@gmail.com>
2021-05-02 21:35:36 +0300

    Initial commit

---
+++ src/main/efes.java
@@ -0,0 +1,13 @@
+package main;
+
+public class efes {
+
+    public static void main(String[] args) {
+        System.out.println("ef");
+        System.out.println(efe(1,2));
+    }
+
+    public static int efe(int a,int b){
+        return a*b;
+    }
+}
---
+++ src/test/testovv.java
@@ -0,0 +1,34 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import main.efes;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+
+public class testovv {
+
+    private efes calculator;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        calculator = new efes();
+    }
+
+    @Test
+    @DisplayName("Simple multiplication should work")
+    public void testMultiply() {
+        assertEquals(20, calculator.efe(4,5),
+                "Regular multiplication should work");
+    }
+
+    @RepeatedTest(5)
+    @DisplayName("Ensure correct handling of zero")
+    public void testMultiplyWithZero() {
+        assertEquals(0, calculator.efe(0,5), "Multiple with zero should be zero");
+        assertEquals(0, calculator.efe(5,0), "Multiple with zero should be zero");
+    }
+}
